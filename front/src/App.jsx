fb4b365144451781f5c78344225501935ab0c6eb
Anurag Khobragae <anuragk2204@gmail.com>
2024-12-03 20:00:15 +0530

    front

---
+++ front/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div className="p-4">
+      <Button>Hello shadcn</Button>
+    </div>
+  )
+}
+
+export default App
